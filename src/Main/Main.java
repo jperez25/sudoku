257cfc751e3020d98007df7240b3ed59757c17c3
jperez25 <jova9991@gmail.com>
2018-05-19 14:09:47 -0500

    Home Screen UI

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package Main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../UI/HomeScreen.fxml"));
+        primaryStage.setTitle("Sudoku");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
