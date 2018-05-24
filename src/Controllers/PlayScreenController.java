60839d014162463cb0ddacd1a615e3bb14ff03e9
jperez25 <jova9991@gmail.com>
2018-05-24 09:30:41 -0500

    Grid class v.2

--- src/Controllers/PlayScreenController.java
+++ src/Controllers/PlayScreenController.java
@@ -2,8 +2,10 @@ package Controllers;
 
 import Main.GridClass;
 import Main.Main;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -26,6 +28,7 @@ public class PlayScreenController {
 
         GridClass gr = new GridClass();
         parent.getChildren().add(gr.getGrid());
+
 
         TheButton.setOnAction(event -> {
             System.out.println(difficulty);
--- src/Main/GridClass.java
+++ src/Main/GridClass.java
@@ -11,15 +11,20 @@ public class GridClass extends GridPane{
     public GridClass(){
         grid = new GridPane();
         grid.setPadding(new Insets(10));
-        grid.setHgap(20);
-        grid.setVgap(20);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setGridLinesVisible(true);
+        grid.setLayoutX(200.0);
+        grid.setLayoutY(150.0);
 
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
                 int number = 9 * r + c;
                 TextField button = new TextField(String.valueOf(number));
+                button.setMaxWidth(40);
                 grid.add(button, c, r);
             }
+
         }
     }
 
